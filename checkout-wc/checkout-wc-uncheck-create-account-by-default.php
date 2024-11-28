9aca998fd570ff8a9def8f2bb64dbd90f03803b9
Ian Misner <ian.misner@gmail.com>
2024-11-28 01:16:36 -0500

    Update checkout-wc-uncheck-create-account-by-default.php

--- checkout-wc/checkout-wc-uncheck-create-account-by-default.php
+++ checkout-wc/checkout-wc-uncheck-create-account-by-default.php
@@ -10,8 +10,4 @@
  * @return bool.
  */
 
-/**
- * Filter source: https://github.com/kestrelwp/checkout-for-woocommerce/blob/0461630163b70d2876aa352f23462f012a845cef/includes/Managers/AssetManager.php#L476.
- */
-
 add_filter( 'cfw_check_create_account_by_default', '__return_false' );
